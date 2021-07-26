4ed160b8eee7849737218940a3fa10a07b3e7efc
Conner Gleason <connereg@optonline.net>
2021-07-26 18:48:31 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+const employee = {
+    name: 'Conner',
+    streetAddress: '88 Starr'
+} 
+
+function updateEmployeeWithKeyAndValue(employee, name, newName) {
+    const updatedEmployee = {...employee };
+    updatedEmployee[name] = newName;
+    return updatedEmployee;
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, newName) {
+    employee[name] = newName;
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, name){
+    const updatedEmployee = {...employee};
+    delete updatedEmployee.name;
+    return updatedEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, name){
+    delete employee.name;
+    return employee;
+}
